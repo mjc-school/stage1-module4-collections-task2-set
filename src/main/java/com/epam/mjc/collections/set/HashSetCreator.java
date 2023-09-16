875ec6b8d4d222f647c4ca2e13b8a76e8c3c2131
Davron <hdavron7@gmail.com>
2023-09-16 13:46:32 +0500

    task2

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,20 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (Integer number : sourceList) {
+            if (number % 2 == 0) {
+                do {
+                    set.add(number);
+                    number /= 2;
+                } while (number % 2 == 0);
+                set.add(number);
+            } else {
+                set.add(number);
+                set.add(2 * number);
+            }
+        }
+        return set;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -1,9 +1,20 @@
 package com.epam.mjc.collections.set;
 
 import java.util.List;
+import java.util.NavigableSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        NavigableSet<Integer> set = new TreeSet<>();
+
+        for (Integer number : sourceList) {
+            number = number * number;
+            if (number >= lowerBound && number <= upperBound) {
+                set.add(number);
+            }
+        }
+        return set;
     }
 }
